d53e334b982941693499e3aa1e4e9e71e95905e3
MarvEltun <eltun178@gmail.com>
2017-10-30 23:12:20 +0200

    Added home buttons to remote control activity

--- app/src/main/java/com/pae14_1/RemoteControlActivity.java
+++ app/src/main/java/com/pae14_1/RemoteControlActivity.java
@@ -2,6 +2,8 @@ package com.pae14_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class RemoteControlActivity extends AppCompatActivity {
 
@@ -9,7 +11,20 @@ public class RemoteControlActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_control);
-        setTitle(this.getString(R.string.rc_robot));
+        setTitle("Remote Control");
 
+        setSupportActionBar((Toolbar) findViewById(R.id.mainToolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

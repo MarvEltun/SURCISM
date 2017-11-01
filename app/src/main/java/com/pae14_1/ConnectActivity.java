8ccdec99c36e1fe176fa37e73c778f7bc4703bcf
Emiraslan <aslanbaxisli.ba@gmail.com>
2017-11-01 09:55:44 +0200

    Added method for enabling the bluetooth

--- app/src/main/java/com/pae14_1/ConnectActivity.java
+++ app/src/main/java/com/pae14_1/ConnectActivity.java
@@ -2,12 +2,19 @@ package com.pae14_1;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Set;
+
+
 public class ConnectActivity extends AppCompatActivity {
     Button blueCntBtn;
+    private BluetoothAdapter myBluetooth = null;
+    private Set pairedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +24,36 @@ public class ConnectActivity extends AppCompatActivity {
         blueCntBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(ConnectActivity.this, HomePageActivity.class);
+                Intent intent = new Intent(ConnectActivity.this, HomePageActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
     }
+
+    public void enablingBluetooth() {
+
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetooth == null)
+        {
+            //Show a mensag. that thedevice has no bluetooth adapter
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            //finish apk
+            finish();
+        }
+        else
+        {
+            if (myBluetooth.isEnabled())
+            { }
+            else
+            {
+                //Ask to the user turn the bluetooth on
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon,1);
+            }
+        }
+
+    }
 }
+
+

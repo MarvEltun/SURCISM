12fe5d8ade67e31dcb33c9534c2da0ad35780489
Emiraslan <aslanbaxisli.ba@gmail.com>
2017-11-01 12:56:13 +0200

    Created Home and Main fragment

---
+++ app/src/main/java/com/pae14_1/FunctionFragments/MainFragment.java
@@ -0,0 +1,12 @@
+package com.pae14_1.FunctionFragments;
+
+/**
+ * Created by aslan on 11/1/2017.
+ */
+
+import android.support.v4.app.Fragment;
+
+public class MainFragment extends  Fragment{
+
+
+}

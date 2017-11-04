b07a53d7d3ab499bbebeaa324936a91d96ac5e3a
MarvEltun <eltun178@gmail.com>
2017-11-05 01:44:33 +0200

    Removed unused code block

--- app/src/main/java/com/pae14_1/HomePageActivity.java
+++ app/src/main/java/com/pae14_1/HomePageActivity.java
@@ -212,55 +212,6 @@ public class HomePageActivity extends AppCompatActivity {
     }
 
 
-    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
-    {
-
-        private ProgressDialog progress;
-        BluetoothAdapter myBluetooth = null;
-        BluetoothSocket btSocket = null;
-        private boolean isBtConnected = false;
-        final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
-        private boolean ConnectSuccess = true; //if it's here, it's almost connected
-
-        @Override
-        protected void onPreExecute() {
-            progress = ProgressDialog.show(HomePageActivity.this, "Connecting...", "Please wait!!!");  //show a progress dialog
-        }
-
-        @Override
-        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
-        {
-            try {
-                if (btSocket == null || !isBtConnected) {
-                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
-                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(Globals.address);//connects to the device's address and checks if it's available
-                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
-                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
-                    btSocket.connect();//start connection
-                }
-            } catch (IOException e) {
-                ConnectSuccess = false;//if the try failed, you can check the exception here
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
-        {
-            super.onPostExecute(result);
-
-            if (!ConnectSuccess) {
-                Toast.makeText(HomePageActivity.this, HomePageActivity.this.getString(R.string.cnt_failed), Toast.LENGTH_LONG).show();
-                HomePageActivity.this.finish();
-            } else {
-                Toast.makeText(HomePageActivity.this, HomePageActivity.this.getString(R.string.connected), Toast.LENGTH_LONG).show();
-
-                isBtConnected = true;
-            }
-            progress.dismiss();
-        }
-    }
 
 
 }
